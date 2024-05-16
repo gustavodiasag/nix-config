1c24a4ade2f6642edbbfddfe7a67ddd78297a3a8
Gustavo Dias de Aguiar <gustavodias.aguiar1@gmail.com>
2024-05-16 11:34:02 -0300

    feat: add initial alacritty configuration

---
+++ home-manager/alacritty.nix
@@ -0,0 +1,63 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  programs.alacritty = {
+    enable = true;
+  };
+
+  programs.alacritty.settings = {
+    font = {
+      size = 12;
+      
+      normal = {
+        family = "JetBrainsMono";
+      };
+    };
+
+    window = {
+      opacity = 0.9;
+      blur = false;
+
+      padding = {
+        x = 1;
+        y = 1;
+      };
+    };
+
+    colors = with config.colorScheme.colors; {
+      bright = {
+        black = "0x${base00}";
+        blue = "0x${base0D}";
+        cyan = "0x${base0C}";
+        green = "0x${base0B}";
+        magenta = "0x${base0E}";
+        red = "0x${base08}";
+        white = "0x${base06}";
+        yellow = "0x${base09}";
+      };
+
+      cursor = {
+        cursor = "0x${base06}";
+        text = "0x${base06}";
+      };
+
+      normal = {
+        black = "0x${base00}";
+        blue = "0x${base0D}";
+        cyan = "0x${base0C}";
+        green = "0x${base0B}";
+        magenta = "0x${base0E}";
+        red = "0x${base08}";
+        white = "0x${base06}";
+        yellow = "0x${base0A}";
+      };
+
+      primary = {
+        background = "0x${base00}";
+        foreground = "0x${base06}";
+      };
+    };
+  };
+}
