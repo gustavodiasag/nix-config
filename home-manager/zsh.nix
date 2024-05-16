588e2f5b8ec004b4657cb82ae1acb82147dc238d
Gustavo Dias de Aguiar <gustavodias.aguiar1@gmail.com>
2024-05-16 11:35:36 -0300

    refac(zsh): change shell theme

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -4,13 +4,27 @@
 } : {
   programs.zsh = {
     enable = true;
+
     enableCompletion = true;
+    enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
+    autocd = true;
+
+    shellAliases = {
+      # General commands
+      g = "git";
+
+      # Nix-related
+      nixswitch = "sudo nixos-rebuild switch";
+
+      # Typos
+      "cd.." = "cd ..";
+    };
 
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
-      theme = "norm";
+      theme = "gozilla";
     };
   };
 }
